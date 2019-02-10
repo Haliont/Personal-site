25f86529e04a09818312ca454519e2e7717e01a4
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-02-10 20:33:06 +0600

    wip

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <!-- <router-view/> -->
+    This site in development. You can see the my
+    <br/>
+    <a href="http://haliont.surge.sh/">old personal site</a>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#app {
+  width: 100%;
+  min-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/StudySolutions.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This page in development</h1>
+  </div>
+</template>
